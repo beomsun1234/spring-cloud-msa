449f8a20e2824c571a0dbf15580457ac13512bb0
Beomsun <beomsun22@gmail.com>
2021-11-02 17:58:13 +0900

    EDIT - dto 수정

--- msa-user-service/src/main/java/com/bs/msauserservice/dto/LoginDto.java
+++ msa-user-service/src/main/java/com/bs/msauserservice/dto/LoginDto.java
@@ -1,5 +1,6 @@
 package com.bs.msauserservice.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,4 +9,10 @@ import lombok.NoArgsConstructor;
 public class LoginDto {
     private String email;
     private String password;
+
+    @Builder
+    public LoginDto(String email , String password){
+        this.email = email;
+        this.password = password;
+    }
 }
--- msa-user-service/src/main/java/com/bs/msauserservice/dto/SignUpDto.java
+++ msa-user-service/src/main/java/com/bs/msauserservice/dto/SignUpDto.java
@@ -2,6 +2,7 @@ package com.bs.msauserservice.dto;
 
 import com.bs.msauserservice.domain.Role;
 import com.bs.msauserservice.domain.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,14 @@ public class SignUpDto {
     private String email;
     private String password;
     private String nickName;
+
+
+    @Builder
+    public SignUpDto(String email, String password, String nickName){
+        this.email = email;
+        this.password = password;
+        this.nickName = nickName;
+    }
 
 
     public User toEntity(){
