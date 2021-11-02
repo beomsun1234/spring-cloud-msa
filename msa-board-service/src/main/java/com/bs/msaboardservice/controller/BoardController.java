b7a8bd1cf9733fb05a8c8aa0ebee96b547ee34ce
Beomsun <beomsun22@gmail.com>
2021-11-02 18:03:29 +0900

    ADD - BoardController

--- msa-board-service/src/main/java/com/bs/msaboardservice/controller/BoardController.java
+++ msa-board-service/src/main/java/com/bs/msaboardservice/controller/BoardController.java
@@ -1,14 +1,47 @@
 package com.bs.msaboardservice.controller;
 
+import com.bs.msaboardservice.dto.BoardCreateDto;
+import com.bs.msaboardservice.dto.BoardInfo;
+import com.bs.msaboardservice.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
-    @GetMapping("/board/hello")
+    private final BoardService boardService;
+
+    @GetMapping("/hello")
     public String home(){
         return "msa-board-service";
     }
+
+    /**
+     * 상세보기
+     * @param id
+     * @return
+     */
+    @GetMapping("board/{id}")
+    public BoardInfo findOneById(@PathVariable Long id){
+        return boardService.findOneById(id);
+    }
+    /**
+     * 글작성
+     */
+    @PostMapping("{id}/board")
+    public Long createBoard(@PathVariable Long id, @RequestBody BoardCreateDto boardCreateDto){
+        return boardService.creatBoard(id, boardCreateDto);
+    }
+    /**
+     * 특정 유저가 작성한 글 보기
+     * @param id
+     * @return
+     */
+    @GetMapping("{id}/board")
+    public List<BoardInfo> home(@PathVariable Long id){
+        return boardService.findBoardsByMemberId(id);
+    }
+
 }
