0ace4a2cb046dc453e5b3e9b4b0d51518839f835
Beomsun <beomsun22@gmail.com>
2021-11-03 19:10:48 +0900

    reply-service

---
+++ msa-reply-service/src/test/java/com/bs/msareplyservice/MsaReplyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.msareplyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaReplyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
