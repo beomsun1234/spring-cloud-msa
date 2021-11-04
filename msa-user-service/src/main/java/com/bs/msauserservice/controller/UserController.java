c397675c117a27b4ad9efc9542f1f528612de9e3
Beomsun <beomsun22@gmail.com>
2021-11-04 18:56:54 +0900

    ADD - findAllById(게시판 상세보기시 댓글 작성한 유저정보 제공하기위해) controller

--- msa-user-service/src/main/java/com/bs/msauserservice/controller/UserController.java
+++ msa-user-service/src/main/java/com/bs/msauserservice/controller/UserController.java
@@ -7,27 +7,55 @@ import com.bs.msauserservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+
     @GetMapping("/user/hello")
     public String home(){
         return "msa-user-service";
     }
 
+    /**
+     * 유저 찾기(게시판 상세보기시 댓글 작성한 유저정보를 제공하기 위해)
+     * @param ids
+     * @return
+     */
+    @GetMapping("/user/reply")
+    public List<UserInfo> findAllByIds(@RequestParam List<Long> ids){
+        return userService.findAllByIds(ids);
+    }
+
+    /**
+     * 유저상세보기
+     * @param id
+     * @return
+     */
     @GetMapping("/user/{id}")
     public UserInfo findOneById(@PathVariable Long id){
         return userService.getUserInfoById(id);
     }
 
+    /**
+     * 로그인
+     * @param id
+     * @return
+     */
     @PostMapping("/auth/login")
     public String findOneById(@RequestBody LoginDto loginDto){
         return userService.logInUser(loginDto);
     }
 
+    /**
+     * 회원가입
+     * @param signUpDto
+     * @return
+     */
     @PostMapping("/auth/join")
     public Long findOneById(@RequestBody SignUpDto signUpDto){
         return userService.signUpUser(signUpDto);
