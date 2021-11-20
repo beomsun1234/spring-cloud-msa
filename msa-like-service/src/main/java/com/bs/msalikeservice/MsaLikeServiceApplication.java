33fe77607d8d7b54607d84bd98aa1f531c75fca3
Beomsun <beomsun22@gmail.com>
2021-11-20 13:23:43 +0900

    ADD - like service

---
+++ msa-like-service/src/main/java/com/bs/msalikeservice/MsaLikeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bs.msalikeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaLikeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaLikeServiceApplication.class, args);
+	}
+
+}
---
+++ msa-like-service/src/test/java/com/bs/msalikeservice/MsaLikeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.msalikeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaLikeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
